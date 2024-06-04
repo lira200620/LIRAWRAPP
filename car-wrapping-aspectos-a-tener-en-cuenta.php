aea1d12ef99ff9a607adb9aaa83e35a19c6cbfed
lira200620 <liraorozco2@gmail.com>
2024-06-04 08:05:18 -0700

    Add files via upload

---
+++ car-wrapping-aspectos-a-tener-en-cuenta.php
@@ -0,0 +1,273 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Car wrapping: 10 aspectos a tener en cuenta en la rotulación de vehículos | PLASTGrommet</title>
+<meta charset="utf-8">
+<meta name="keywords" content="car wrapping, vinilado de vehículos, alternativa a la pintura, vinilar coches "/>
+<meta name="description" content="El car wrapping es una técnica de rotulación de vehículos que sirve para decorar y personalizar automóviles, barcos, aviones o motocicletas. Se emplea como alternativa a la pintura."> 
+<link rel="stylesheet" href="./css/reset.css?v9" type="text/css" media="all">
+<link rel="stylesheet" href="./css/layout.css" type="text/css" media="all">
+<link rel="stylesheet" href="./css/style.css?v9.2" type="text/css" media="all">
+<script type="text/javascript" src="./js/jquery-1.6.js" ></script>
+<script type="text/javascript" src="./js/tabs.js"></script>
+<script type="text/javascript" src="source/helpers/jquery.fancybox-media.js?v=1.0.5"></script>
+<script type="text/javascript" src="./lib/jquery-1.8.2.min.js"></script>
+<script type="text/javascript" src="./source/jquery.fancybox.js?v=2.1.3"></script>
+<link rel="stylesheet" type="text/css" href="./source/jquery.fancybox.css?v=2.1.2" media="screen" />
+<script type="text/javascript" src="./lib/jquery.mousewheel-3.0.6.pack.js"></script>
+<link rel="stylesheet" type="text/css" href="./source/helpers/jquery.fancybox-buttons.css?v=1.0.5" />
+<script type="text/javascript" src="./source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
+<link rel="stylesheet" type="text/css" href="./source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" />
+<script type="text/javascript" src="./source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+<script type="text/javascript" src="./source/helpers/jquery.fancybox-media.js?v=1.0.5"></script>
+<script type="text/javascript">
+		$(document).ready(function() {
+						$('.fancybox-media')
+				.attr('rel', 'media-gallery')
+				.fancybox({
+					openEffect : 'none',
+					closeEffect : 'none',
+					prevEffect : 'none',
+					nextEffect : 'none',
+					arrows : false,
+					helpers : {
+						media : {},
+						buttons : {}
+					}});
+			$(".manual-12").click(function() {$.fancybox.open('../images/ojaladora-automatica-evolution.jpg');});
+});
+</script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-26119006-4', 'plastgrommet.com');
+  ga('send', 'pageview');
+
+</script>
+
+<!-- Global site tag (gtag.js) - Google Analytics PARA SEGUIMIENTO EN ANALYTICS DE PLASTGROMMET.COM en general-->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-26119006-16"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-26119006-16');
+</script>
+	</head>
+<body id="page2">
+
+<!-- Google Tag Manager -->
+<noscript><iframe src='//www.googletagmanager.com/ns.html?id=GTM-TXV79Q'
+height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TXV79Q');</script>
+<!-- End Google Tag Manager -->
+
+<!-- Start of HubSpot Embed Code --> 
+
+  <script type='text/javascript' id='hs-script-loader' async defer src='//js.hs-scripts.com/4338245.js'></script> 
+
+<!-- End of HubSpot Embed Code --> 
+
+<div class="body2"><div class="body5"><div class="main">
+<header class="menu2"><div class="wrapper">
+		<nav>
+			<ul id="menu">
+				<li id="nav1"><a class="enlacenav1" href="http://plastgrommet.com/es/"></a></li>
+				<li id="nav4"><a href="http://plastgrommet.com/es/Herramientas-y-accesorios-para-rotulistas.php">Nuestros<span>Productos</span></a></li>
+				<li id="nav2"><a href="http://plastgrommet.com/es/sobre-nosotros.php">Qui&eacute;nes<span>somos</span></a></li>
+				<li id="nav3"><a href="http://plastgrommet.com/es/noticias-plastgrommet.php">Noticias<span>PLASTGrommet</span></a></li>
+				<li id="nav5"><a href="http://plastgrommet.com/es/area-profesionales.php">ÁREA <span>PROFESIONALES</span></a></li>
+				<li id="nav6"><a href="http://plastgrommet.com/es/pdfs/catalogo-general-PLASTGrommet-es.pdf">Descarga<span>Nuestro cat&aacute;logo</span></a></li>
+				<li id="nav7"><a href="http://plastgrommet.com/es/formulario-contacto.php">Contacta<span>con nosotros</span></a></li>
+				<li id="nav8"></li></ul></nav>
+				<div class="confianza">
+				<a style="float:left;" href="https://plastgrommet.com/es/" title="PLASTGrommet, soluciones para el acabado en lonas y textiles"><img src="http://plastgrommet.com/es/images/enfocado.jpg" alt="Soluciones para el acabado en lonas y textiles"></a>
+				<a class="right bandera" href="https://plastgrommet.com/us/"><img class="img_center" src="./images/flag-us.png" title="English version" alt="english version"></a>
+				<a class="right bandera" href="https://plastgrommet.com/en/"><img class="img_center" src="./images/flag-uk.png" title="English version" alt="english version"></a>
+				<a class="right bandera" href="https://plastgrommet.com/es/"><img class="img_center" src="./images/flag-es.png" title="Version en castellano" alt="version en castellano"></a>
+				
+				
+</div></header></div></div></div>
+<div class="body3">
+<div class="main"><article id="content"><div class="wrapper">
+<section class="col12 pad_bot1">
+<center>
+<h1 class="new-tools" line-height=1>Car wrapping: 10 aspectos a tener en cuenta en la rotulación de vehículos</h1>
+</center>
+<p>
+El <b>car wrapping es una técnica</b> para decorar, personalizar y ‘pintar’ utilizada en automóviles, barcos, aviones, motocicletas y muchos otros soportes. Se emplea un <b>vinilo especial adhesivo</b> que recubre total o parcialmente el vehículo, moldeando manualmente el material que se adhiere sobre la pintura.
+</p>
+<center><a href="https://plastgrommet.com/es/kits-herramientas-de-rotulacion-para-rotulista.php" target="_blank"><img src="https://plastgrommet.com/es/images/news/car-wrapping-worker-plastgrommet.jpg" alt="herramientas rotulación" title="Rotulación con kit de herramientas"></a></center></p>
+<p>
+Se le conoce también como <a href="https://plastgrommet.com/es/que-es-vinilado-de-vehiculos-alternativa-a-la-pintura.php">vinilado de vehículos</a>, aunque su forma anglosajona está muy extendida.
+</p>
+<p>
+Al ser un trabajo manual, se requiere una formación especializada para colocar el vinilo de manera correcta y sin imperfecciones. Además, debido a las múltiples posibilidades que ofrece, es una técnica muy usada con fines publicitarios, siendo una opción que causa un gran impacto a la vista de las personas.
+</p>
+<br>
+<hr>
+<br>
+<h2 class="new-tools">Herramientas utilizadas en la técnica de car wrapping</h2>
+<p>
+El éxito de la técnica de rotulación de vehículos reside en tres aspectos fundamentales: la calidad de los materiales o película a utilizar, el uso de las herramientas adecuadas y la experiencia del aplicador profesional.
+</p>
+<p>A continuación veremos las herramientas clave que debe tener todo carrocero para realizar una aplicación de vinilo perfecta.</p>
+<h3 class="new-tools">1. Espátulas</h3>
+<p>
+Es el elemento principal y posiblemente uno de los más importantes. Se necesitan diferentes tipos y durezas para realizar un correcto aplicado en las diferentes zonas del vehículo. Por ejemplo, para las curvaturas y zonas irregulares las espátulas deben ser flexibles.
+</p>
+<a href="https://plastgrommet.com/es/herramientas-de-rotulacion/espatulas-y-rasquetas-aplicacion-vinilos.php" target="_blank" ><img src="http://plastgrommet.com/es/images/news/tipos-de-espatulas-azul.jpg" alt="espátulas plástico" title="Estpátulas de plástico"></a><br><br>
+<p>
+En la elección de las <a href="http://plastgrommet.com/es/herramientas-de-rotulacion/espatulas-y-rasquetas-aplicacion-vinilos.php">espátulas</a> para trabajar, también debemos tener en cuenta el vinilo o película con la que trabajamos. Existen vinilos delicados, muy <b>usados en car wrapping como el chrome, que necesitan espátulas específicas</b> para que, al aplicar el material, no llegue a dañarlo. En este caso en concreto, recomendamos el uso de espátulas con un lado recubierto de microfibra que permite el uso en aplicaciones en seco o mojado y ayuda a que se deslicen mejor.
+</p>
+<center><a href="http://plastgrommet.com/es/herramientas-de-rotulacion/espatulas-y-rasquetas-aplicacion-vinilos.php"><< Descubre Marathon, espátulas con recubrimiento doble de microfibra que evita daños en el vinilo  >></a></center><br><br>
+
+<h3 class="new-tools">2. Imanes</h3>
+<p>
+Es la herramienta magnética perfecta para el aplicador de vinilos. Permite sujetar el material para mantenerlo posicionado en la zona de aplicación. Son unas manos extras de trabajo que evitan errores al posicionar el material.
+El modelo más usado es el de <a href="http://plastgrommet.com/es/herramientas-de-rotulacion/herramientas-magneticas-para-rotulistas.php">puño magnético</a>.
+</p>
+
+<h3 class="new-tools">3. Pistola de aire caliente</h3>
+<p>Se le conoce también como decapador o pistola de calor y sirve para poder manipular el vinilo, eliminar la memoria y fijarlo en la posición definitiva. La pistola puede llegar a unos 80 grados, por lo que es imprescindible contar con guantes para manejar la película.</p>
+
+<h3 class="new-tools">4. Cutters</h3>
+<p>Esta herramienta debe ser de altísima calidad para poder cortar sin necesidad de realizar una excesiva presión y no dañar la pintura del vehículo. Es importante contar con recambios de cuchilla para mantener el afilado óptimo.</p>
+
+<h3 class="new-tools">5. Guantes</h3>
+<p>
+Los guantes son esenciales para mantener un trabajo limpio y sin impurezas. Siempre tomando en cuenta, que al trabajar con materiales plásticos y con adhesivos, los guantes deben tener características especiales.
+Los guantes para la rotulación de vehículos más recomendados son los modelos antiestáticos y sin costuras, muy cómodos de usar.
+</p> 
+<h3 class="new-tools">6. Limpiador de superficies</h3>
+<p>
+Antes de empezar un proceso de vinilado, es elemental realizar una limpieza de toda la superficie para eliminar todo tipo de suciedad, cera, silicona o partículas que no permitan una buena aplicación de la película autoadhesiva. Los ribetes, esquinas y bordes son los puntos críticos para la adhesión, por lo que se aconseja revisar la limpieza de estos espacios.
+</p>
+
+<h3 class="new-tools">7. Bolígrafo anti-burbujas</h3>
+<p>
+Es posible la aparición de burbujas durante la aplicación, por lo que será necesario contar con un bolígrafo anti-burbujas, una herramienta diseñada especialmente para este uso. Si se tiende a utilizar un cutter para eliminar las burbujas, con el paso del tiempo aparecerá una raja.
+</p>
+
+<h3 class="new-tools">8. El tipo de vinilo</h3>
+<p>
+En el mercado podemos encontrarnos distintos tipos de vinilos: monométricos, poliméricos o fundidos (conocidos también como ‘cast’). Para car wrapping se recomienda el vinilo fundido por ofrecer mejores resultados, mayor durabilidad, flexibilidad y adaptación a diferentes formas. Igualmente, trabajar con este tipo de vinilo es muy fácil, ya que muchos tienen la capacidad de ser reposicionable.
+</p>
+
+<h3 class="new-tools">9. Herramientas para retirar el vinilo</h3>
+<p>
+Para poder retirar el vinilo se recomiendan cuchillas de plástico y cuñas que levanten la película.
+También se utilizan productos químicos que ayudan a extraer la película y son respetuosos con la pintura del vehículo.
+Para trabajos más intensos, los discos de goma montados en taladros son muy populares.
+</p> 
+
+<h3 class="new-tools">10. Errores comunes en la rotulación</h3>
+<p>
+Por último, podemos tener fallos en la aplicación de la película por varios motivos. Al no elegir las herramientas correctas o los materiales adecuados podemos entorpecer el trabajo profesional, y al no tener una adecuada combinación de impresora/laminadora el trabajo puede generar residuos indeseados.
+</p>
+<p>
+Otro error común es no contar con un almacenaje de vinilos correcto en el taller, por lo que el material puede sufrir daños irreparables. Una buena organización y <a href="http://plastgrommet.com/es/Bastidores-almacenamiento-rollos-vinilo.php">almacenaje de vinilos</a> hace que el trabajo en el taller sea más fluido y sin desperdicios.
+</p>
+<br>
+<hr>
+<br>
+<h2 class="new-tools">Ventajas de la rotulación de vehículos</h2>
+<p>
+El car wrapping tiene infinitas posibilidades de decoración y personalización exclusiva. A diferencia del barnizado, tiene un menor coste y más rapidez, además de permitir la protección de las superficies originales del vehículo. Asimismo, la facilidad de retirada permite transformar los vehículos en una pieza publicitaria nueva. Para los publicistas es una opción ideal por su alto impacto estético y de alta calidad.
+</p>
+
+<center>
+<a href="https://plastgrommet.com/es/que-es-vinilado-de-vehiculos-alternativa-a-la-pintura.php"><img src="http://plastgrommet.com/es/images/news/vinilado-de-vehiculos.jpg" alt="vinilado de vehículos" title="Vinilado de vehículos: Qué es y por qué es una alternativa a la pintura"></a></center></p>
+<br><hr>
+<p><center>
+<a href="http://plastgrommet.com/es/formulario-contacto.php"><img src="http://plastgrommet.com/es/images/news/CTA-contacto.png"></a>
+</center>
+</p>
+<br><br><br><br>
+</section>
+
+
+
+<section height="550px" class="col22 pad_left1 pad_bot1">
+
+<center><h2 class="under">Ultimas noticias</h2></center>
+	      <a target="_blank" href="http://plastgrommet.com/es/tipos-de-espatulas-industria-grafica.php">
+		      <img width="100%" src="http://plastgrommet.com/es/images/news/tipos-de-espatulas.jpg" alt="Tipos de espátulas utilizadas en la industria gráfica">
+		   </a>
+		   <br><br><br>
+	      <a target="_blank" href="http://plastgrommet.com/es/problemas-soluciones-movilidad-rollos-de-vinilo.php">
+		      <img width="100%" src="http://plastgrommet.com/es/images/news/problemas-movilidad-vinilos.jpg" alt="Soluciones para la movilidad y almacenaje">
+		   </a>
+		   <br><br><br>
+		   <a target="_blank" href="http://plastgrommet.com/es/como-colocar-banner-lonas-publicitarias.php">
+		      <img width="100%" src="http://plastgrommet.com/es/images/news/colocacion-banners-pvc.jpg" alt="Lonas publicitarias">
+		   </a>
+			<br><br>
+</section>
+
+
+
+
+</article></div></div>
+</section>
+<div class="body4"><div class="main"><article id="content2">
+				<div class="wrapper">
+					<section class="col3 pad_left4">
+						<h4>PLASTGrommet</h4>
+						<ul class="list1">
+							<li><a href="http://plastgrommet.com/es/sobre-nosotros.php">Empresa</a></li>
+							<li><a href="http://plastgrommet.com/es/formulario-contacto.php">Contacto</a></li>
+							<li><a href="http://plastgrommet.com/es/sitemap.php">Sitemap</a></li>
+							<li><a href="http://plastgrommet.com/es/aviso-legal-plastgrommet.php" rel="nofollow">Aviso legal</a></li>
+							<li><a href="http://plastgrommet.com/es/politica-de-cookies-plastgrommet.php" rel="nofollow">Pol&iacute;tica de cookies</a></li>
+							<li><a href="http://plastgrommet.com/es/politica-de-privacidad-plastgrommet.php" rel="nofollow">Pol&iacute;tica de privacidad</a></li>
+							<li><a href="http://plastgrommet.com/es/subenciones-ayudas.php">Subvenciones y ayudas</a></li>
+							</ul></section>
+					<section class="col3 pad_left4">
+						<h4>Contacto</h4>
+						<ul class="address">
+							<li><img src="http://plastgrommet.com/es/images/telefono-plastgrommet.PNG" title="Contacta con nosotros" alt="telefono empresa">+34 965 339 969</li>
+							<li><img src="http://plastgrommet.com/es/images/fax-plastgrommet.PNG" title="Envia tu fax" alt="fax de empresa">+34 965 330 078</li>
+							<li><img src="http://plastgrommet.com/es/images/email-plastgrommet.PNG" title="Envia tus pedidos" alt="Email de empresa">
+							<a href="" rel="nofollow" onclick="this.href='mailto:' + 'info' + '@' + 'plastgrommet.com'" title="correo electronico">Contacta con nosotros</a></li>
+							<li><img src="http://plastgrommet.com/es/images/direccion-plastgrommet.png" title="localizacion empresa" alt="direccion de PLASTGrommet">Carretera Font Roja 23 03801 Alcoy (Alicante) Espa&ntilde;a</li>
+					</ul>
+					</section>
+					<section class="col3 pad_left4">
+						<h4>Horario</h4>
+						<ul id="horario">
+							<li>Lunes a Jueves</li>
+							<li>08:30 - 17:00</li>
+							<li>Viernes de 8:00 a 14:00</li>
+						</ul>
+
+						
+					</section>
+					<section class="col3 right">
+						<h4>S&iacute;guenos</h4>
+						<ul id="icons">
+							<li><a href="http://www.youtube.com/user/PlastGrommet"><img title="Canal PLASTGrommet" src="http://plastgrommet.com/es/images/canal-youtube-plastgrommet.jpg" alt="Nuesto canal youtube plastgrommet">Canal Youtube</a></li>
+							<li><a href="http://es.linkedin.com/company/plastgrommet"><img title="LinkedIn PLASTGrommet" src="http://plastgrommet.com/es/images/linkedin-plastgrommet.jpg" alt="Nuestro LinkedIn de empresa">LinkedIn</a></li>
+						</ul>
+						</section>
+				</div>
+				<br>
+				<div class="logos-footer">
+				<center>
+				<a href="https://www.aimplas.net/" target="_blank"><img title="Miembro de AIMPLAS" alt="Miembro de AIMPLAS" src="https://plastgrommet.com/es/images/footer/aimplas-logo-es.png"></a>  <a href="https://www.printingunited.com/" target="_blank"><img title="PRINTING UNITED ALLIANCE member" alt="PRINTING UNITED ALLIANCE member" src="https://plastgrommet.com/es/images/footer/Printing-United-member.jpg"></a>  <a href="https://ciencia.sede.gob.es/" target="_blank"><img title="Reconocimiento PYME INNOVADORA" alt="Reconocimiento PYME INNOVADORA"src="https://plastgrommet.com/es/images/footer/PYME-INNOVADORA.jpg"></a>
+				</center>
+				</div>
+				<br>
+			</article>
+			<footer>&copy; PLASTGROMMET S.L.U.</footer></div></div>
+</body>
+</html>
